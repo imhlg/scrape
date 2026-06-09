bfecfecec547fbcc4a9d534f5094b46e5e75d2ac
imhlg <achalg@outlook.com>
2026-06-09 22:12:33 +0530

    edited all_Spans

--- scrape.py
+++ scrape.py
@@ -7,16 +7,16 @@ print(type(resp))
 xsoup = BeautifulSoup(resp.text, 'html.parser')
 print(type(xsoup))
 
-# print(xsoup.find_all('a'))
+#   print(xsoup.find_all('a'))
 all_Spans = xsoup.find_all('span', class_='score')
-all_Spans_alt = xsoup.select('a')
-print(all_Spans_alt)
+#   all_Spans_alt = xsoup.select('a')
+print(all_Spans)
 
 li_len = len(all_Spans)
 scores, i = [], 0
 while i < li_len:
     scores.append(all_Spans[i].text)
-    #print(type(scores[i]))
+    print(scores[i])
     i += 1
 # scores = all_As
 print(scores[0])
