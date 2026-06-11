b30423019720ec9a3a5f87f108e66fa5bd2d9b99
imhlg <achalg@outlook.com>
2026-06-12 00:36:42 +0530

    finished without sorting

--- scrape.py
+++ scrape.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import pprint
 
 resp = requests.get('https://news.ycombinator.com/news')
 print(type(resp))
@@ -9,18 +10,27 @@ print(type(xsoup))
 
 all_scores = xsoup.select('.score')
 all_links = xsoup.select('.titleline')
-print(type(all_links),'\n')
+print(type(all_links), '\n')
 
-dict_Results = {}
+dict_Results, li_scores, FnResult1 = {}, [], []
 
-def GEtNAmesLInks():
-    li_len = len(all_links)
-    result, i = [], 0
-    while i < li_len:
-        dict_Results.update({all_links[i].text:all_links[i].find('a').get('href')})
-        i += 1
-    return
 
+def articleofhighpoints(links, score):
+    FnResult, xpoint = [], 0
+    for idx, link in enumerate(links):
+        xpoint = int(score[idx].getText().replace(' points', ''))
+        if xpoint >= 100:
+            #            li_scores.append(score[idx].getText().strip(' points'))
+            #            dict_Results.update({links[idx].text: links[idx].find('a').get('href')})
+            FnResult.append({"Title is ": link.text, "URL is ": link.find('a').get('href'), "with Points ": xpoint})
+    return FnResult
+
+
+FnResult1 = articleofhighpoints(all_links, all_scores)
+
+pprint.pprint(FnResult1)
+
+'''print(FnResult)
 
 def GEtSCores():
     # greater than 100 score points in a list
@@ -32,8 +42,4 @@ def GEtSCores():
         i += 1
     return
 
-
-GEtSCores()
-GEtNAmesLInks()
-
-print(dict_Results[2])
+GEtSCores()'''
