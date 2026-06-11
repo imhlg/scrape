3fbace329b49a8f7dc9faf7c2131c3814078dc93
imhlg <achalg@outlook.com>
2026-06-11 20:02:10 +0530

    adde links and names finding code

--- scrape.py
+++ scrape.py
@@ -7,23 +7,33 @@ print(type(resp))
 xsoup = BeautifulSoup(resp.text, 'html.parser')
 print(type(xsoup))
 
-#   print(xsoup.find_all('a'))
-all_Spans = xsoup.find_all('span', class_='score')
-#   all_Spans_alt = xsoup.select('a')
-print(all_Spans)
+all_scores = xsoup.select('.score')
+all_links = xsoup.select('.titleline')
+print(type(all_links),'\n')
 
-li_len = len(all_Spans)
-scores, i = [], 0
-while i < li_len:
-    scores.append(all_Spans[i].text)
-    print(scores[i])
-    i += 1
-# scores = all_As
-print(scores[0])
+dict_Results = {}
 
-
-# from spck and 2
-
-def scoremorethan100():
-    #    for score in all_Spans:
+def GEtNAmesLInks():
+    li_len = len(all_links)
+    result, i = [], 0
+    while i < li_len:
+        dict_Results.update({all_links[i].text:all_links[i].find('a').get('href')})
+        i += 1
     return
+
+
+def GEtSCores():
+    # greater than 100 score points in a list
+    li_len = len(all_scores)
+    scores, i = [], 0
+    while i < li_len:
+        scores.append(all_scores[i].text)
+        # print(scores[i])
+        i += 1
+    return
+
+
+GEtSCores()
+GEtNAmesLInks()
+
+print(dict_Results[2])
